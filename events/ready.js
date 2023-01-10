a1ae53a0b877130fea66ed2be8c9d94a67183811
scriptor.js <65604210+shadowforce78@users.noreply.github.com>
2023-01-10 01:02:52 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,3 +3,7 @@ const client = require("../index");
 client.on("ready", () =>
     console.log(`${client.user.tag} is up and ready to go!`)
 );
+
+client.on('ready', () => {
+    client.user.setActivity('Some activity', { type: 'PLAYING' });
+})
